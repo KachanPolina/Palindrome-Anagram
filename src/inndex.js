c44ea63d0b2ec1457ca668f3d8ffcb30b206bb75
Polina Kachan <kachan.polina8@gmail.com>
2026-04-21 17:45:44 +0300

    Add function isPalindromHard

--- src/inndex.js
+++ src/inndex.js
@@ -9,3 +9,21 @@ function isPalindromSimple(string) {
 
 console.log(isPalindromSimple('Я несУ ГУСЕня'));
 console.log(isPalindromSimple('сонЦе'));
+
+console.log('--------------')
+// Palindrom (hard)
+function isPalindromHard(string) {
+    let stringBlankSpace = string.toLowerCase().replaceAll(' ', ''); 
+    let reversedString = "";
+    for (let i = stringBlankSpace.length - 1; i >= 0; i--) {
+        reversedString += stringBlankSpace[i];
+    }
+    if (reversedString === stringBlankSpace) return true;
+    return false;
+}
+
+console.log(isPalindromHard('Я несУ ГУСЕня'));
+console.log(isPalindromHard('сонЦе'));
+
+console.log('--------------')
+

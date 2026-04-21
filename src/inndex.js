e29df8ba68a85c76b8e3407f704b7f81f666fe94
Polina Kachan <kachan.polina8@gmail.com>
2026-04-21 17:50:37 +0300

    Add finction isAnagramSimple

--- src/inndex.js
+++ src/inndex.js
@@ -11,6 +11,7 @@ console.log(isPalindromSimple('Я несУ ГУСЕня'));
 console.log(isPalindromSimple('сонЦе'));
 
 console.log('--------------')
+
 // Palindrom (hard)
 function isPalindromHard(string) {
     let stringBlankSpace = string.toLowerCase().replaceAll(' ', ''); 
@@ -26,4 +27,17 @@ console.log(isPalindromHard('Я несУ ГУСЕня'));
 console.log(isPalindromHard('сонЦе'));
 
 console.log('--------------')
+
+// Anagram (simple)
+function isAnagram(string1, string2) {
+    const string1LowerCase = string1.toLowerCase().replaceAll(' ', ''); 
+    const string2LowerCase = string2.toLowerCase().replaceAll(' ', ''); 
+    const arr1 = Array.from(string1LowerCase).sort().join("");
+    const arr2 = Array.from(string2LowerCase).sort().join("");
+    if (arr1 === arr2) return true;
+    return false;
+}
+
+console.log(isAnagram('ArT        ', '         Rat'));
+console.log(isAnagram('Hello', 'Sea'));
 
